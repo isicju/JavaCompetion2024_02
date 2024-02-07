893abb6a3d38319f11813d1e1d6a48959e825dc1
icju <isicju@gmail.com>
2024-02-07 17:06:32 +0000

    web server

---
+++ webserer/src/main/java/com/example/Generator.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import java.io.*;
+import java.net.*;
+
+public class Generator {
+    public static void main(String[] args) throws IOException {
+        int port = Integer.valueOf(args[0]);
+
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server listening on port " + port);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+            Thread thread = new Thread(new RequestHandler(clientSocket));
+            thread.start();
+        }
+    }
+
+    private static class RequestHandler implements Runnable {
+        private final Socket clientSocket;
+
+        public RequestHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                writer.println("HTTP/1.1 200 OK");
+                writer.println("Content-Type: text/plain");
+                writer.println();
+                writer.println("Hello");
+
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+}
