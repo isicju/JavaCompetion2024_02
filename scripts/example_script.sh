6a23b6277aac83b83a6a66d828b9f454bbc5370b
icju <isicju@gmail.com>
2024-02-29 01:21:24 +0000

    draft

--- scripts/example_script.sh
+++ scripts/example_script.sh
@@ -1,4 +1,5 @@
-mvn clean -DjarName=example install  -f ../default/pom.xml
-mvn clean -DjarName=example2 install  -f ../default/pom.xml
+rm -rf ../default/target/*
+mvn -DjarName=example install  -f ../default/pom.xml
+mvn -DjarName=example2 install  -f ../default/pom.xml
 java -jar ../default/target/example-jar-with-dependencies.jar ../generator/data.txt
 java -jar ../default/target/example2-jar-with-dependencies.jar ../generator/data.txt
