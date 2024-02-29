eb9e02bf3a9da73c7d477cf9aa8857fdc24ea708
icju <isicju@gmail.com>
2024-02-29 01:19:58 +0000

    draft

---
+++ default/src/kotojava/competition/example2.java
@@ -0,0 +1,78 @@
+package kotojava.competition;
+
+import lombok.AllArgsConstructor;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+import static java.lang.Double.parseDouble;
+
+@AllArgsConstructor
+public class example2 {
+
+    //todo put your name and solution type
+    private static String participantName = "Dmitrii";
+    private static ParticipantExperience experience = ParticipantExperience.AFTER_JUNIOR;
+    private static SolutionType solutionType = SolutionType.REGULAR;
+
+    enum ParticipantExperience {
+        BEFORE_JUNIOR, AFTER_JUNIOR
+    }
+
+    enum SolutionType {
+        REGULAR, ADVANCED
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("please specify data.txt location with all records.");
+        } else {
+            File file = new File(args[0]);
+            if (!file.exists()) {
+                System.out.println("file: " + file.getAbsoluteFile() + " not found");
+            } else {
+                calculateStats(file);
+            }
+        }
+    }
+
+    //todo change implementation
+    private static void calculateStats(File file) throws IOException {
+        List<String> cityRecords = Files.readAllLines(file.toPath());
+        double meanDistance = 0, maxDistance = 0, minDistance = Double.MAX_VALUE;
+        String maxCity = "", minCity = "";
+
+        for (String record : cityRecords) {
+            String[] parts = record.split("[/|]");
+            String cityName = parts[1];
+            double x = parseDouble(parts[2]);
+            double y = parseDouble(parts[3]);
+            double distance = distance(x, y);
+            meanDistance += distance;
+
+            if (distance > maxDistance) {
+                maxDistance = distance;
+                maxCity = cityName;
+            }
+            if (distance < minDistance) {
+                minDistance = distance;
+                minCity = cityName;
+            }
+        }
+        meanDistance = meanDistance / (double) cityRecords.size();
+
+        printResults(minCity, maxCity, meanDistance);
+    }
+
+    private static void printResults(String minCity, String maxCity, double meanDistance) {
+        System.out.println("min|max|mean|name|group");
+        System.out.println(minCity + "|" + maxCity + "|" + meanDistance + "|" + participantName + "|" + experience + "|" + solutionType);
+    }
+
+    private static double distance(double x, double y) {
+        return Math.sqrt(x * x + y * y);
+    }
+
+}
--- scripts/example_script.sh
+++ scripts/example_script.sh
@@ -1,2 +1,4 @@
 mvn clean -DjarName=example install  -f ../default/pom.xml
+mvn clean -DjarName=example2 install  -f ../default/pom.xml
 java -jar ../default/target/example-jar-with-dependencies.jar ../generator/data.txt
+java -jar ../default/target/example2-jar-with-dependencies.jar ../generator/data.txt
